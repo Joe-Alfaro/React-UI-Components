f5149780d591b2c7d81d8ad427a9316cc8b88ff7
joe-alfaro <josephlalfaro@gmail.com>
2019-03-18 15:57:19 -0700

    start styling for project 1

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Header.css';
 
 const HeaderTitle = () => {
-  return <h1>Lambda School @LambdaSchool - 26 Jan </h1>
+  return <h1 className="header-title">Lambda School @LambdaSchool - 26 Jan </h1>
 };
 
 export default HeaderTitle;
