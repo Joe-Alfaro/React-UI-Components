22b0efb1d1e784af61c5437a057f71f651e1ed06
joe-alfaro <josephlalfaro@gmail.com>
2019-03-19 15:57:21 -0700

    completed mvp of making react calculator and styling

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,9 @@
 import React from 'react';
+
 import './Button.css';
+
+const ActionButton = props => {
+  return <button class="action-button">{props.children}</button>
+};
+
+export default ActionButton;
